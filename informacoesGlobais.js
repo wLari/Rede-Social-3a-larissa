a49305e0637409e5545f59b2a3ffc6c5c95e6e4c
wLari <163433120+wLari@users.noreply.github.com>
2024-10-25 10:47:23 -0300

    lari

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,3 +1,7 @@
 const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json' 
 
-console.log(url);
+async function visualizarinformacoesGlobais() {
+    const res = await fetch(url) 
+    const dados = res.json()
+    console.log(dados)
+}
