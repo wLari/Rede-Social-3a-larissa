9a7972fdf5353d0ae2c2579ddaefab37b6863eb2
wLari <163433120+wLari@users.noreply.github.com>
2024-11-22 08:04:40 -0300

    coff

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -3,5 +3,12 @@ const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-glo
 async function visualizarinformacoesGlobais() {
     const res = await fetch(url) 
     const dados = res.json()
-    console.log(dados)
-}
+    console.log(dados);
+    const paragrafo = document.createElement('p') 
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Vocẽ sabia o mundo que tem ${dados.total_pessoas_mundo} de pessoas e
+    que aoroximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e
+    passam em media ${dados.tempo_medio} horas conectadas.'
+    console.log(paragrafo)
+
+vizualizarinformacoesGlobais()
